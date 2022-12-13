c757d6cc53cfeb97c682fea6fd8072f73303be9c
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2022-12-13 22:02:28 +0100

    Add context for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
-import "../styles/globals.css"
-
-export default function Home() {
-  return <h1>Welcome to my todos app !</h1>
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-600">Welcome to my todos app !</h1>
+    </>
+  )
 }
+
+export default App
