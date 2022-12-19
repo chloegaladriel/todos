b1caa3635df612e9c93793827a160a2b887c2dea
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2022-12-19 18:57:35 +0100

    fix: add state in index.js file to create new todo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,33 @@
+import { useState } from "react"
+import Navbar from "../components/Navbar"
+import TodoListTab from "../components/TodoListTab"
+import todosInitialState from "./todosInitialState"
+
 const App = () => {
+  const [state, setState] = useState(todosInitialState)
+
+  const handleAddTodoList = (name) => {
+    const getLastId = () => {
+      return state.todoLists[state.todoLists.length - 1].id
+    }
+    setState({
+      ...state,
+      todoLists: [
+        ...state.todoLists,
+        {
+          name,
+          todos: {},
+          id: getLastId() + 1,
+        },
+      ],
+    })
+  }
+
   return (
     <>
-      <h1 className="text-red-600">Welcome to my todos app !</h1>
+      <Navbar state={state} handleAddTodoList={handleAddTodoList} />
+
+      <TodoListTab />
     </>
   )
 }
