087c716589bc00ee759a97ac4aa3f28a1d6e8f18
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2022-12-21 19:53:45 +0100

    fix: add validationSchema and yup packages

--- src/components/AddTodoListForm.jsx
+++ src/components/AddTodoListForm.jsx
@@ -1,17 +1,24 @@
 import { Form, Formik } from "formik"
+import * as yup from "yup"
 import Button from "./Button"
 import FormField from "./FormField"
 
 const AddTodoListForm = (props) => {
   const { onSubmit } = props
+
   const initialValues = {
     name: "",
   }
+
   const handleSubmit = (values, { resetForm }) => {
     onSubmit(values.name)
 
     resetForm()
   }
+
+  const validationSchema = yup.object({
+    description: yup.string().min(1),
+  })
 
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
@@ -23,7 +30,11 @@ const AddTodoListForm = (props) => {
           type="text"
         />
 
-        <Button type="submit" variant="submit">
+        <Button
+          type="submit"
+          variant="submit"
+          validationSchema={validationSchema}
+        >
           SUBMIT
         </Button>
       </Form>
