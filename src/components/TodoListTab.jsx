a94305168fd037b3a89683529b03999040f27f59
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2022-12-21 19:36:56 +0100

    feat: add checked param

--- src/components/TodoListTab.jsx
+++ src/components/TodoListTab.jsx
@@ -19,9 +19,9 @@ const TodoListTab = (props) => {
               <input
                 type="checkbox"
                 className="ml-4 my-2 h-4 w-4 appearance-none rounded-none border border-slate-200 default:bg-white checked:bg-green-400"
+                checked={todo.done}
               />
               {todo.description}
-              {todo.done.length}
               <Button variant="icon" className="ml-auto">
                 <TrashIcon className="h-6 w-6 hidden group-hover:block" />
               </Button>
