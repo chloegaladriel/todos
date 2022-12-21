74ae0dbe4745735dcf24e7657d4b930d6d425ca0
Chloé <91722427+chloegaladriel@users.noreply.github.com>
2022-12-21 18:49:34 +0100

    feat: add dynamic remaining todos filter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,26 +4,22 @@ import Modal from "./Modal"
 const Header = (props) => {
   const { state, handleAddTodoList, setIsOpen, isOpen, onClick } = props
 
-  //const remainingTodos = Object.entries(
-  //state.todoLists[state.todoLists.length - 1].todos
-  //).filter((todo) => todo.done === false).length
-
   return (
     <div className="flex overflow-scroll sticky top-0 bg-white">
       {state.todoLists.map((todoList, todoListIndex) => (
         <Button
-          key={todoList.id}
           hasBorderFirst
+          key={todoList.id}
           onClick={() => {
             onClick(todoListIndex)
           }}
         >
           {todoListIndex}-{todoList.name}-{todoList.id}
           <span className="relative ml-4 pr-2 pl-2 -mr-9 bg-green-400 rounded-full z-10">
-            {todoList.completedTodos}
+            {todoList.todos.filter(({ done }) => done).length}
           </span>
           <span className="relative ml-4 pr-2 pl-6 bg-blue-400 rounded-full z-0">
-            {todoList.totalTodos}
+            {todoList.todos.length}
           </span>
         </Button>
       ))}
--- src/pages/todosInitialState.js
+++ src/pages/todosInitialState.js
@@ -3,62 +3,37 @@ const todosInitialState = {
     {
       id: 1,
       name: "Shopping",
-      totalTodos: 4,
-      completedTodos: 2,
-      todos: {
-        1: {
-          description: "Mushrooms",
-          done: true,
-        },
-        2: {
-          description: "Salad",
-          done: true,
-        },
-        3: {
+      todos: [
+        { id: 0, description: "Mushrooms", done: true },
+        { id: 1, description: "Salad", done: true },
+        {
+          id: 2,
           description: "Watermelon",
           done: false,
         },
-        4: {
-          description: "Coconut",
-          done: false,
-        },
-      },
+        { id: 3, description: "Coconut", done: false },
+      ],
     },
     {
       id: 2,
       name: "Homework",
-      totalTodos: 2,
-      completedTodos: 1,
-      todos: {
-        1: {
+      todos: [
+        {
+          id: 0,
           description: "Mathematics",
           done: true,
         },
-        2: {
-          description: "JavaScript",
-          done: false,
-        },
-      },
+        { id: 1, description: "JavaScript", done: false },
+      ],
     },
     {
       id: 3,
       name: "XmasGift",
-      totalTodos: 3,
-      completedTodos: 0,
-      todos: {
-        1: {
-          description: "Macbook",
-          done: false,
-        },
-        2: {
-          description: "Playstation",
-          done: false,
-        },
-        3: {
-          description: "Wine glass",
-          done: false,
-        },
-      },
+      todos: [
+        { id: 0, description: "Macbook", done: false },
+        { id: 1, description: "Playstation", done: false },
+        { id: 2, description: "Wine glass", done: false },
+      ],
     },
   ],
 }
